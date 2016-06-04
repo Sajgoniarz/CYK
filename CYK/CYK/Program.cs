02805b308845517cd69f5c609b6b561258f471da
Marek <Sajgoniarz1@gmail.com>
2016-06-04 11:13:17 +0200

    Release v1

---
+++ CYK/CYK/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CYK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Rule> G = new List<Rule>();
+
+            G.Add(new Rule('S', "AC"));
+            G.Add(new Rule('C', "SB"));
+            G.Add(new Rule('S', "AB"));
+            G.Add(new Rule('A', "a"));
+            G.Add(new Rule('B', "b"));
+
+            string word = "aaabbb";
+
+            CYK parser = new CYK(word, G);
+
+            parser.Parse();
+            parser.PrintTable();
+
+            string not = "";
+
+            if (!parser.GetResult())
+                not = "'t";
+
+            Console.WriteLine("\"{0}\" can" + not + " be produced!", word);
+        }
+    }
+}
---
+++ CYK/CYK/Rule.cs
@@ -0,0 +1,29 @@
+﻿namespace CYK
+{
+    class Rule
+    {
+        public char S;
+        private string Nt;
+
+        public Rule(char s, string n)
+        {
+            S = s;
+            Nt = n;
+        }
+
+        public bool Check(char c)
+        {
+            for (int i = 0; i < Nt.Length; i++)
+                if (c == Nt[i])
+                    return true;
+            return false;
+        }
+
+        public bool Check(Rule X, Rule Y)
+        {
+            if (Nt[0] == X.S && Nt[1] == Y.S)
+                return true;
+            return false;
+        }
+    }
+}
